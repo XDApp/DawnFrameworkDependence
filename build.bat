cee2e10cea66d6ac8436e97908bc5132f8703aca
SkyZH <iSkyZH@gmail.com>
2015-05-30 11:02:34 +0800

    Add libpng Building Support

--- build.bat
+++ build.bat
@@ -11,4 +11,8 @@ cd ..
 msbuild /property:Configuration="Release" /property:Platform=x64 ".\glfw-3.1.1\build\GLFW.sln"
 copy ".\glfw-3.1.1\build\src\Release\glfw3.lib" "..\Library"
 echo GLFW has been built successfully!
+msbuild /property:Configuration="Release" /property:Platform=x64 ".\lpng1617\projects\vstudio\vstudio.sln"
+copy ".\lpng1617\projects\vstudio\x64\Release Library\libpng16.lib" "..\Library"
+copy ".\lpng1617\projects\vstudio\x64\Release Library\zlib.lib" "..\Library"
+echo libpng has been built successfully!
 pause
